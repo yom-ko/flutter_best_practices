ed986fcabd97e4a6730652ed478b46659849e762
yomko <artem.getcode@gmail.com>
2025-01-10 23:05:59 +0100

    Improve opt 2

--- lib/home_opt_2.dart
+++ lib/home_opt_2.dart
@@ -4,10 +4,10 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-// 2. Avoid rebuilding some widgets repetitively using `const` keyword.
+// 2. Avoid rebuilding certain widgets repetitively using `const` keyword.
 // Refs:
 // https://blog.codemagic.io/how-to-improve-the-performance-of-your-flutter-app./
-// https://habr.com/ru/articles/502882/#ne-ispolzuyte-vidzhet-opacity-v-animaciyah
+// https://habr.com/ru/articles/502882/#ispolzuyte-const
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,14 +17,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _random = Random();
   Color _currentColor = Colors.grey;
 
-  final Random _random = Random();
-
   void _onPressed() {
-    int randomNumber = _random.nextInt(30);
     setState(() {
-      _currentColor = Colors.primaries[randomNumber % Colors.primaries.length];
+      _currentColor = Colors.primaries[_random.nextInt(30) % Colors.primaries.length];
     });
   }
 
@@ -54,21 +52,12 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-// BAD
-// class _BackgroundWidget extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     print('building `_BackgroundWidget`');
-//     return Image.network(
-//       'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg',
-//       fit: BoxFit.cover,
-//     );
-//   }
-// }
-
-// GOOD
 class _BackgroundWidget extends StatelessWidget {
+  // GOOD with `const` constructor
+  // _BackgroundWidget doesn't rebuild when the color changes.
   const _BackgroundWidget();
+  // BAD without `const` constructor
+  // _BackgroundWidget rebuilds when the color changes.
 
   @override
   Widget build(BuildContext context) {
