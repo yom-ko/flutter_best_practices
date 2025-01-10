5b926ebae01e33b99c54f79f53b66c6e3270a789
yomko <artem.getcode@gmail.com>
2025-01-10 22:56:51 +0100

    Improve opt 1

--- lib/home_opt_1.dart
+++ lib/home_opt_1.dart
@@ -6,133 +6,77 @@ import 'package:flutter/material.dart';
 // Refs:
 // https://blog.codemagic.io/how-to-improve-the-performance-of-your-flutter-app./
 // https://habr.com/ru/articles/502882/#ne-ispolzuyte-vidzhet-opacity-v-animaciyah
-// https://youtu.be/IOyq-eTRhvo
+// https://www.youtube.com/watch?v=IOyq-eTRhvo
 // https://stackoverflow.com/questions/53234825/what-is-the-difference-between-functions-and-classes-to-create-reusable-widgets
 // https://github.com/flutter/flutter/issues/19269
 // https://pub.dev/packages/functional_widget
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  Widget build(context) {
+    return CounterScope(
+      notifier: Counter(),
+      child: const HomeBody(),
+    );
+  }
 }
 
-class _HomePageState extends State<HomePage> {
-  void _onPressed() {
-    setState(() {});
-  }
+class HomeBody extends StatelessWidget {
+  const HomeBody({super.key});
 
   @override
   Widget build(BuildContext context) {
+    print('build `HomeBody`');
     return Scaffold(
+      body: const Center(
+        // GOOD
+        // HomeBody doesn't rebuild when the counter changes.
+        child: Title(),
+        // BAD
+        // HomeBody rebuilds when the counter changes.
+        // child: title(context),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _onPressed,
-        child: const Icon(Icons.colorize),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            // BAD
-            _buildHeader(),
-            _buildMain(context),
-            _buildFooter(),
-            // GOOD
-            const _Header(),
-            const _Main(),
-            const _Footer(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildHeader() {
-    print('building `Header` from method');
-    const size = 40.0;
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
-      child: CircleAvatar(
-        backgroundColor: Colors.grey,
-        radius: size,
-        child: FlutterLogo(
-          size: size,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildMain(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Colors.grey[700],
-        child: Center(
-          child: Text(
-            'Hello Flutter',
-            style: Theme.of(context).textTheme.headlineLarge,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildFooter() {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text('This is the footer '),
-    );
-  }
-}
-
-class _Header extends StatelessWidget {
-  const _Header();
-
-  @override
-  Widget build(BuildContext context) {
-    print('building `_Header` widget');
-    const size = 40.0;
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
-      child: CircleAvatar(
-        backgroundColor: Colors.grey,
-        radius: size,
-        child: FlutterLogo(
-          size: size,
-        ),
+        onPressed: () => CounterScope.of(context, listen: false).value++,
+        child: const Icon(Icons.add),
       ),
     );
   }
 }
 
-class _Main extends StatelessWidget {
-  const _Main();
+class Title extends StatelessWidget {
+  const Title({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Colors.grey[700],
-        child: Center(
-          child: Text(
-            'Hello Flutter',
-            style: Theme.of(context).textTheme.headlineLarge,
-          ),
-        ),
-      ),
-    );
+    print('build `Title` from widget');
+    return Text('Counter ${CounterScope.of(context).value}');
   }
 }
 
-class _Footer extends StatelessWidget {
-  const _Footer();
+Widget title(BuildContext context) {
+  print('build `Title` from function');
+  return Text('Counter ${CounterScope.of(context).value}');
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text('This is the footer '),
-    );
+class CounterScope extends InheritedNotifier {
+  const CounterScope({
+    super.key,
+    required super.child,
+    required super.notifier,
+  });
+
+  static Counter of(BuildContext context, {bool listen = true}) {
+    if (listen) {
+      return context.dependOnInheritedWidgetOfExactType<CounterScope>()!.notifier as Counter;
+    } else {
+      return context.getInheritedWidgetOfExactType<CounterScope>()!.notifier as Counter;
+    }
   }
+}
+
+class Counter extends ValueNotifier<int> {
+  Counter() : super(0);
 }
